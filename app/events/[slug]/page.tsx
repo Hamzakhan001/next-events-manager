3ea53750bc992ec93eeedc9147ceedde7495b07b
Hamza <hamza@Hamzas-MacBook.local>
2026-02-13 16:56:36 +0000

    events detail page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
@@ -6,16 +7,39 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
 
     const { slug } = await params
     const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-    const { event } = await request.json()
+    const { event: { description, image, overview, date, time, location, agenda, mode, audience, tags } } = await request.json()
 
-    if (!event) {
+    if (!description) {
         return notFound()
     }
 
-
     return (
         <section id="event">
-            <h1>Event Details: <br />{slug}</h1>
+            <div className="header">
+                <h1>Event Description</h1>
+                <p className="mt-2">{description}</p>
+            </div>
+            <div className="details">
+                {/* Left side- Event Content */}
+                <div className="content">
+                    <Image src={image} className="banner" alt="Event Banner" width={800} height={800} />
+
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <h2>{overview}</h2>
+                    </section>
+
+                    <section className="flex-col-gap-2">
+                        <h2>Event Details:</h2>
+
+                    </section>
+                </div>
+
+                {/* Right side- Booking Form */}
+                <aside className="booking">
+                    <p className="text-lg font-semibold">Book Event</p>
+                </aside>
+            </div>
         </section>
     )
 }
