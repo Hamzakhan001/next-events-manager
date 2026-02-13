745e3149887be914fa2166f0885601af447419d3
Hamza <hamza@Hamzas-MacBook.local>
2026-02-13 12:23:55 +0000

    events page and card changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
-import { title } from "process"
-
-
-const events = [
-  { image: '/images/event1.png', title: 'Event 1' },
-  { image: '/images/event2.png', title: 'Event 2' },
-  { image: '/images/event1.png', title: 'Event 1' },
-  { image: '/images/event1.png', title: 'Event 1' },
-]
+import events from "@/lib/constants"
 
 const page = () => {
   return (
@@ -18,7 +10,6 @@ const page = () => {
       </h1>
       <p>Meetups,Hackathons and Conferences all at one place</p>
       <ExploreBtn />
-
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
